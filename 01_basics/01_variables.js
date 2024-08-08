f9c969dd2be78ad0aa77a29660e22418a372dddd
srujan7m <srujanmmattur@gmail.com>
2024-08-08 20:28:39 +0530

    Add files via upload

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,7 +4,7 @@ var accountPassword = "12345"
 accountCity = "Jaipur"
 let accountState;
 
-// accountId = 2 // not allowed
+// accountId = 2  not allowed constant change nahi hoga //
 
 
 accountEmail = "hc@hc.com"
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+//alert( a + b) // will not run bcs we r running in node js not in any browser
+
+ console.log( 3 + 3);
+ console.log("srujan") 
+
+  // code readability should be high
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//DATATYPES:
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => ex line 13
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //output: undefined
+console.log(typeof null); // object
+
