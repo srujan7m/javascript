48d014bf4748ee209d27d60a49bef250b90c53ea
srujan7m <srujanmmattur@gmail.com>
2024-08-08 19:40:27 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var instead use let 
+because of issue in block scope and functional scope
+also blank considered to be as a variable but not to prefer
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
